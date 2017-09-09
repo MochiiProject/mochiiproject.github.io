26a9b7263c7a171d2fab56697ec0494a5f122bf8
Yuin Ee <yee@linkedin.com>
2017-09-09 11:51:07 -0700

    Post 'Void Seeker' video, add sticky menu, add side menu for non-desktop devices, tweak stylings, renamed images

--- js/main.js
+++ js/main.js
@@ -8,15 +8,18 @@
   "use strict";
 
   $(document).ready(function () {
-		//Disable cut copy paste
+		// Disable cut copy paste
     $('img').bind('cut copy paste', function (e) {
       e.preventDefault();
     });
    
-    //Disable mouse right click
+    // Disable mouse right click
     $('img').on("contextmenu",function(e){
       return false;
     });
+
+    // Set up side nav (mobile, tablets)
+    $('.button-collapse').sideNav();
   });
 
 }(window.jQuery);
